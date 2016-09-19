65f218576eb3290f87edfb808c1815463b8c0bdc
Fredrik Säfsten <fredrik@wakakuu.com>
2016-09-19 09:29:22 +0200

    Adding db-model and stuffs

---
+++ db-model.php
@@ -0,0 +1,112 @@
+<?php
+
+ini_set("log_errors", 1);
+ini_set("error_log", "plantbot.log");
+
+class Db_model {
+
+    function db_connect($forceDB = false) {
+        static $connection;
+
+        if ($forceDB) echo "FORCING SELECT DB! (Probably due to re-deployed db)\n";
+
+        if (!isset($connection) || $forceDB) {
+            $ini = parse_ini_file('settings.ini');
+            $connection = mysqli_connect($ini['db_server'], $ini['db_user'], $ini['db_password']);
+            if ($connection === false) {
+                die (mysqli_error());
+            }
+            mysqli_select_db($connection, $ini['db_name']);
+        }
+        return $connection;
+    }
+
+    function queryDb($query) {
+        //error_log($query);
+        $connection = $this->db_connect();
+        $result = mysqli_query($connection, $query);
+        if (!$result) {
+            die(mysqli_error($connection));
+        }
+        return $result;
+    }
+
+    function getPlants() {
+        $query = "SELECT * from plants where active=1";
+        $result = $this->queryDb($query);
+        $plants = array();
+        while ($row = mysqli_fetch_assoc($result)) {
+            $plants[] = $row;
+        }
+        return $plants;
+    }
+
+    function saveData($data) {
+        print_r($data);
+    }
+
+    function updateLastFeedTime($data) {
+        $query = "UPDATE plants set last_feed_time='".$data['last_feed_time']."' where id=".$data['id'];
+        $result = $this->queryDb($query);
+
+    }
+
+    function createPlant($name, $nickname, $location, $kind, $feedIntervalAprox, $soilTreshold, $feedVolume, $feedMode) {
+        $query = "INSERT INTO plants set name='$name', nickname='$nickname', location='$location', kind='$kind', feed_interval_aprox=$feedIntervalAprox, soil_treshold=$soilTreshold, feed_volume=$feedVolume, feed_mode='$feedMode', birthday=NOW(), last_feed_time=NOW(), active=1;";
+        $result = $this->queryDb($query);
+    }
+
+    function createDatabase() {
+
+        $query = "DROP DATABASE IF EXISTS plantbot;";
+        $result = $this->queryDb($query);
+
+        $query = "CREATE DATABASE plantbot;";
+        $result = $this->queryDb($query);
+        
+        $connection = $this->db_connect($forceDB = true);
+
+        $query = "CREATE TABLE plants  ( 
+                                            id int auto_increment,
+                                            name varchar(52),
+                                            nickname varchar(52),
+                                            birthday date,
+                                            location varchar(52),
+                                            kind varchar(52),
+                                            feed_interval_aprox int,
+                                            soil_treshold int,
+                                            feed_volume int,
+                                            feed_mode varchar(52),
+                                            last_feed_time datetime,
+                                            active bool,
+                                            primary key (id)
+                                        );";
+        $result = $this->queryDb($query);
+        
+        $query = "CREATE TABLE plantdata    (   
+                                                id int auto_increment,
+                                                plant_id int,
+                                                soil int,
+                                                time datetime,
+                                                temp float,
+                                                humidity float,
+                                                pressure float,
+                                                day_of_year int,
+                                                clouds float,
+                                                age_days int,
+                                                time_since_last_feeding_hours int,
+                                                primary key(id)
+                                            );";
+        $result = $this->queryDb($query);
+    }
+
+    function setup() {
+        $this->createDatabase();
+        
+        $feedIntervalAprox = 3; // Days?
+        $soilTreshold = 83;
+        $feedVolume = 300; // Centiliters? Per feeding time?
+        $feedMode = 'soilTemp';
+        $this->createPlant('Plant 1', 'Janne', 'Indoors', 'Fredskalla', $feedIntervalAprox, $soilTreshold, $feedVolume, $feedMode);
+    }
+}
--- plantbot.php
+++ plantbot.php
@@ -2,91 +2,92 @@
 ini_set("log_errors", 1);
 ini_set("error_log", "plantbot.log");
 
-require_once __DIR__ . '/www/OpenWeatherMap-PHP-Api/Examples/bootstrap.php';
 use Cmfcmf\OpenWeatherMap;
 use Cmfcmf\OpenWeatherMap\Exception as OWMException;
 
-// Todo: Pusha till git
-// Todo: Lägg till OpenWeatherMap som submodule?
+require 'vendor/autoload.php';
 
-//Todo: DB-structure and setup routine
-$query = "DROP DATABASE IF EXISTS plantbot;";
-$query = "CREATE DATABASE plantbot;";
-$query = "CREATE TABLE plants ( id int autoincrement,
-                                name varchar(52),
-                                ...
-                                primary key (id)";
-$query = "CREATE TABLE data (   id int autoincrement,
-                                plant_id int,
-                                ...
-                                primary key(id)";
+$ini = parse_ini_file('settings.ini');
+$myApiKey = $ini['api_key'];
+
+include('db-model.php');
+$db = new Db_model;
+//$db->setup();
 
 $lang = 'se';
 $units = 'metric';
-
-$plantData = array( 'name' => 'Plant 1',
-                    'nickname' => 'Janne',
-                    'birthday' => '2016-09-09',
-                    'location' => 'Indoors',
-                    'kind' => 'Fredskalla',
-                    'feed_interval_aprox' => 3,
-                    'soil_treshold' => 83,
-                    'feed_volume' => 100,
-                    'feed_mode' => 'soilTemp',
-                    'last_feed_time' => '2016-09-11 18:00',
-                    'img_url' => '',
-                  );
-
-$data = array();
 
 $owm = new OpenWeatherMap();
 $owm->setApiKey($myApiKey);
 $weather = $owm->getWeather('Mölndal', $units, $lang);
 $clouds = $weather->clouds->getValue();
 
-$data['soil'] = (int)getMoistLevel();
-$data['name'] = $plantData['name'];
-$data['time'] = date('Y-m-d H:i');
-$data['temp'] = (int)getTemperature();
-$data['humidity'] = (int)getHumidity();
-$data['pressure'] = (int)getPressure(); // Todo: Saknar sensor för detta va?
-$data['day_of_year'] = date('z')+1;
-$data['clouds'] = $clouds;
-$data['age_days'] = floor((time() - strtotime($plantData['birthday'])) / (60 * 60 * 24));
-
-if ($plantData['location'] == 'Indoors') {
-    if ($data['soil'] <= $plantData['soil_treshold']) {
-        // Todo: Ska matning bara tillåtas vissa tider?
-        // Todo: Vattna
-        error_log("Vattnar ".$plantData['nickname']." med ".$plantData['feed_volume']." cl vatten.");
-        $data['time_since_last_feeding_hours'] = 0;
-        $plantData['last_feed_time'] = date('Y-m-d H:i');
-    } else {
-        $data['time_since_last_feeding_hours'] = floor((time() - strtotime($plantData['last_feed_time'])) / (60 * 60));
+$plants = $db->getPlants();
+foreach ($plants as $plantData) {
+    $data = array();
+    $data['plant_id'] = $plantData['id'];
+    $data['soil'] = (int)getMoistLevel();
+    $data['time'] = date('Y-m-d H:i');
+    $data['temp'] = (int)getTemperature();
+    $data['humidity'] = (int)getHumidity();
+    $data['pressure'] = (int)getPressure(); // Todo: Saknar sensor för detta va?
+    $data['day_of_year'] = date('z')+1;
+    $data['clouds'] = $clouds;
+    $data['age_days'] = floor((time() - strtotime($plantData['birthday'])) / (60 * 60 * 24));
+    
+    if ($plantData['location'] == 'Outdoors') {
+        // More sensors and data?
+        echo $temp = $weather->temperature->getValue();
+        echo $humidity = $weather->humidity->getValue();
+        echo $pressure = $weather->pressure->getValue();
+        echo $weather->wind->speed->getValue();
+        echo $weather->wind->direction->getValue();
+        echo $weather->wind->direction->getUnit();
+        echo $weather->wind->direction->getDescription();
+        echo $weather->precipitation->getValue();
+        echo $weather->precipitation->getDescription();
     }
-} else {
-    /* Outdoor Weather stuffs
-    echo $temp = $weather->temperature->getValue();
-    echo $humidity = $weather->humidity->getValue();
-    echo $pressure = $weather->pressure->getValue();
-    echo $weather->wind->speed->getValue();
-    echo $weather->wind->direction->getValue();
-    echo $weather->wind->direction->getUnit();
-    echo $weather->wind->direction->getDescription();
-    echo $weather->precipitation->getValue();
-    echo $weather->precipitation->getDescription();
-    */
-}
 
-print_r($plantData);
-print_r($data);
-//Todo: Spara data till db
+    if ($plantData['location'] == 'Indoors') {
+        if ($data['soil'] <= $plantData['soil_treshold']) {
+            $watered = waterPlant($plantData);
+            $data['time_since_last_feeding_hours'] = 0;
+            $plantData['last_feed_time'] = date('Y-m-d H:i');
+            $db->updateLastFeedTime($plantData);
+        } else {
+            $data['time_since_last_feeding_hours'] = floor((time() - strtotime($plantData['last_feed_time'])) / (60 * 60));
+        }
+    } else {
+        /* Outdoor Weather stuffs
+        */
+    }
+
+    //Todo: Spara data till db
+    $db->saveData($data);
+}
+echo "-- PLANTS --\n";
+print_r($plants);
+
+//Todo: Kika på Larvel?
+//Todo: Fixa en setup.php
 //Todo: Update plantdata
 //Todo: Pusha data till predictionmodellen
 //Todo: Ta en bild per dag
 //Todo: Statuspage/Dashboard eller mail?
 //Todo: Bevaka low-water-supply?
 //Todo: Flasha lampa i lägenheten?
+
+function waterPlant($plantData) {
+    // Todo: Ska matning bara tillåtas vissa tider?
+    
+    // Todo: Vattna !!!
+    $ok = trim(shell_exec('python waterPlant.py "'.$plantData['feed_volume'].'"'));
+    if ($ok) {
+        error_log("Vattnade ".$plantData['nickname']." med ".$plantData['feed_volume']." cl vatten.");
+    }
+    var_dump($ok); die;
+    return $ok;
+}
 
 function getMoistLevel() {
     return shell_exec('python moistLevel.py');
