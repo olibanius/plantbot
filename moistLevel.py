9a748092edb47ec28fd63644b7a447d29417eb32
Fredrik Säfsten <fredrik@wakakuu.com>
2016-11-20 14:45:58 +0100

    Adding arguments to moistlevel and waterplant

--- moistLevel.py
+++ moistLevel.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python
 
-import smbus
-import time
+import smbus, time, sys
+
+if len(sys.argv) == 1:
+	quit("Too few arguments");
+
+sensorNr = int(sys.argv[1]);
 
 bus = smbus.SMBus(1)
 
-bus.write_byte_data(0x48,0x40 | ((0) & 0x03), 0)
+bus.write_byte_data(0x48,0x40 | ((sensorNr) & 0x03), 0)
 v = bus.read_byte(0x48)
+v = bus.read_byte(0x48) # Double read, got the same reading as last time before.. weird
 print v
--- waterPlant.py
+++ waterPlant.py
@@ -1,8 +1,13 @@
 import sys;
 #exit(0);
 
-import time
+import time, sys
 import RPi.GPIO as GPIO
+
+if len(sys.argv) == 1:
+	quit("Too few arguments");
+
+execTime = float(sys.argv[1]);
 
 # Next we setup the pins for use!
 GPIO.setmode(GPIO.BCM)
@@ -13,7 +18,7 @@ GPIO.setup(18,GPIO.OUT)
 # Makes the motor spin one way for 2 seconds
 GPIO.output(17, True)
 GPIO.output(18, False)
-time.sleep(2)
+time.sleep(execTime)
 
 GPIO.output(17, False)
 GPIO.output(18, False)
